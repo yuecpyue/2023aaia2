83b36bf8b36143bd16473a93eff9cac9ed82987c
yuecpyue <a0985837774@gmail.com>
2024-05-13 11:27:46 +0800

    week12

--- week12/week12-1.cpp
+++ week12/week12-1.cpp
@@ -1,1 +1,20 @@
+#include <stdio.h>
+int isPrime(int n){
+    for(int i=2;i<n;i++){
+        if(n%i==0)
+            return 0;
+    }
+    return 1;
+}
+int main()
+{
+    int n;
+    printf("請輸入一個數字:");
+    scanf("%d",&n);
+    if(isPrime(n))
+        printf("n是質數");
+    else
+        printf("n不是質數");
 
+
+}
